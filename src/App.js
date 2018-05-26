3719f0e1fb1feef8f4f11ff29c14ad7458e7d072
amangotchu <aman.gottu@gmail.com>
2018-05-25 22:01:50 -0500

    Removed Firebase Auth Creds

--- src/App.js
+++ src/App.js
@@ -10,12 +10,7 @@ class App extends Component {
 
   componentWillMount(){
     const config = {
-      apiKey: 'AIzaSyDFzS55p-JePrrYk5Srt6yCn-CDYv9UAGw',
-      authDomain: 'manager-bbf76.firebaseapp.com',
-      databaseURL: 'https://manager-bbf76.firebaseio.com',
-      projectId: 'manager-bbf76',
-      storageBucket: '',
-      messagingSenderId: '295485873501'
+      
     };
     firebase.initializeApp(config);
       }
