815b4205ac1e902661929076d2474afcf572d050
amangotchu <aman.gottu@gmail.com>
2018-05-26 20:49:48 -0500

    Reduxified Firebase Login with exceptions and helper methods

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import ReduxThunk from 'redux-thunk';
 import reducers from './reducers';
 import firebase from 'firebase';
 import LoginForm from './components/LoginForm';
+
+const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
 
 class App extends Component {
 
@@ -20,9 +23,10 @@ class App extends Component {
     firebase.initializeApp(config);
       }
 
+
   render(){
     return (
-      <Provider store={createStore(reducers)}>
+      <Provider store={store}>
         <LoginForm />
       </Provider>
     );
