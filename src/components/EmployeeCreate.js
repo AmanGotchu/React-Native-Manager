1262421ed0fae5d35f09ef01bca347111be720cd
amangotchu <aman.gottu@gmail.com>
2018-05-27 02:57:48 -0500

    Added Picker Component with M-F Choices. Reduxified the Picker

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
+import { Picker } from 'react-native';
 import { Card, CardSection, Input, Button } from './common';
 import { employeeUpdate } from '../actions';
 import { connect } from 'react-redux';
+
 
 class EmployeeCreate extends Component{
   render(){
@@ -23,6 +25,22 @@ class EmployeeCreate extends Component{
           value={this.props.phone}
           onChangeText={value => this.props.employeeUpdate({prop: 'phone', value})}
         />
+        </CardSection>
+
+        <CardSection>
+          <Picker
+           style={{ flex: 1 }}
+           selectedValue={this.props.shift}
+           onValueChange={value => this.props.employeeUpdate({prop: 'shift', value})}
+          >
+            <Picker.Item label="Monday" value="Monday" />
+            <Picker.Item label="Tuesday" value="Tuesday" />
+            <Picker.Item label="Wednesday" value="Wednesday" />
+            <Picker.Item label="Thursday" value="Thursday" />
+            <Picker.Item label="Friday" value="Friday" />
+            <Picker.Item label="Saturday" value="Saturday" />
+            <Picker.Item label="Sunday" value="Sunday" />
+          </Picker>
         </CardSection>
 
         <CardSection>
