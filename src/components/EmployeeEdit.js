42ebc857adc34d0efc4dd4cab6b8d36796155703
amangotchu <aman.gottu@gmail.com>
2018-05-28 03:58:01 -0500

    Added employee delete action creator. Hooked up to Employee Edit component and finished onAccept callback function for modal

--- src/components/EmployeeEdit.js
+++ src/components/EmployeeEdit.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import { Card, CardSection, Button, Confirm } from './common';
 import EmployeeForm from './EmployeeForm';
-import { employeeUpdate, employeeSave } from '../actions';
+import { employeeUpdate, employeeSave, employeeDelete } from '../actions';
 import { connect } from 'react-redux';
 import Communications from 'react-native-communications';
 import _ from 'lodash';
@@ -24,6 +24,16 @@ class EmployeeEdit extends Component {
     const { phone, shift } = this.props;
 
     Communications.text(phone, `Your upcoming shift is on ${shift}`);
+  }
+
+  onAccept() {
+    const { uid } = this.props.employee;
+    this.props.employeeDelete({uid});
+    this.setState({showModal: false});
+  }
+
+  onDecline() {
+    this.setState({showModal: false});
   }
 
   render() {
@@ -50,6 +60,8 @@ class EmployeeEdit extends Component {
 
         <Confirm
           visible={this.state.showModal}
+          onAccept={this.onAccept.bind(this)}
+          onDecline={this.onDecline.bind(this)}
         >
           Are you sure you want to delete this?
         </Confirm>
@@ -64,4 +76,4 @@ const mapStateToProps = (state) => {
   return { name, phone, shift };
 };
 
-export default connect(mapStateToProps, { employeeUpdate, employeeSave })(EmployeeEdit);
+export default connect(mapStateToProps, { employeeUpdate, employeeSave, employeeDelete })(EmployeeEdit);
